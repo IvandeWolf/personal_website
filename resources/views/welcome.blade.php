3aa7df9a2848337fec107e0951f58c2d6edfe921
IvandeWolf <ivan.dewolf1997@gmail.com>
2020-03-03 10:17:51 +0100

    :speech_balloon: Create a frontpage with CV info

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,64 +4,14 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Ivan de Wolf</title>
 
         <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,700&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="{{ asset('css/all.css') }}">
 
         <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     </head>
     <body>
         <div class="flex-center position-ref full-height">
@@ -80,21 +30,193 @@
             @endif
 
             <div class="content">
-                <div class="title m-b-md">
-                    Laravel
+                <div class="title">
+                    Ivan&nbsp;<span class="bold">de&nbsp;Wolf</span>
+                </div>
+                <div class="position">
+                    Computer Science
+                </div>
+                <div class="address">
+                    Veenendaal, The Netherlands
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="tel:+31622419027">
+                        <i class="fas fa-mobile"></i>
+                        06 22419027
+                    </a>
+                    <a href="mailto:ivan.dewolf1997@gmail.com">
+                        <i class="fas fa-envelope"></i>
+                        ivan.dewolf1997@gmail.com
+                    </a>
+                    <a href="https://github.com/IvandeWolf" target="_blank">
+                        <i class="fab fa-github"></i>
+                        IvandeWolf
+                    </a>
+                    <a href="https://www.linkedin.com/in/IvandeWolf" target="_blank">
+                        <i class="fab fa-linkedin"></i>
+                        IvandeWolf
+                    </a>
+                </div>
+
+                <div class="quote">
+                    "All our dreams can come true, if we have the courage to pursue them." - Walt Disney
+                </div>
+
+                <!-- <div class="see_more"><i class="fas fa-chevron-down"></i></div> -->
+            </div>
+        </div>
+
+        <div class="flex-center position-ref" style="background-color: light-grey;">
+            <div class="section">
+                <div class="section_title">
+                    <span>Edu</span>cation<div class="section_title--bar"></div>
+                </div>
+
+                <div class="item">
+                    <div class="item_header">
+                        <div class="item_title">
+                            University of Amsterdam
+                            <div class="location">Amsterdam, The Netherlands</div>
+                        </div>
+                        
+                        <div class="item_subtitle">
+                            BSc Computer Science
+                            <div class="datetime">sep. 2018 - PRESENT</div>
+                        </div>
+                    </div>
+
+                    <ul>
+                        <li>Currently enrolled in honours program with interest in physics courses.</li>
+                        <li>Working at the university as a Teaching assistant and a Tutor.</li>
+                    </ul>
+                </div>
+
+                <div class="item">
+                    <div class="item_header">
+                        <div class="item_title">Hogeschool van Arnhem en Nijmegen</div>
+                        <div class="item_subtitle">HBO-ICT</div>
+                    </div>
+                    <div class="datetime">sep. 2017 - aug. 2018</div>
+
+                    <ul>
+                        <li>Propedeuse achieved</li>
+                    </ul>
                 </div>
             </div>
+
+            <div class="section">
+                <div class="section_title">
+                    <span>Cer</span>tificates<div class="section_title--bar"></div>
+                </div>
+
+                <div class="item">
+                    <div class="item_header">
+                        <div class="item_title">
+                            Korteweg-de Vries Instituut voor Wiskunde
+                            <div class="location">Amsterdam, The Netherlands</div>
+                        </div>
+                        
+                        <div class="item_subtitle">
+                            Mathematics
+                            <div class="datetime">jul. 2018</div>
+                        </div>
+                    </div>
+
+                    <ul>
+                        <li>Certificate for Mathematics obtained by following a summer course.</li>
+                    </ul>
+                </div>
+            </div>
+
+            <div class="section">
+                <div class="section_title">
+                    <span>Exp</span>erience<div class="section_title--bar"></div>
+                </div>
+
+                <div class="item">
+                    <div class="item_header">
+                        <div class="item_title">
+                            University of Amsterdam
+                            <div class="location">Amsterdam, The Netherlands</div>
+                        </div>
+                        
+                        <div class="item_subtitle">
+                            Tutor and Teaching Assistant
+                            <div class="datetime">aug. 2019 - PRESENT</div>
+                        </div>
+                    </div>
+
+                    <ul>
+                        <li>Mentoring a group of 15 students in their academic skills.</li>
+                        <li>Teaching Assistant for different courses as Computer Architecture, Linear Algebra and Web Technology.</li>
+                    </ul>
+                </div>
+
+                <div class="item">
+                    <div class="item_header">
+                        <div class="item_title">
+                            Circle Leisure Development
+                            <div class="location">Veenendaal, The Netherlands</div>
+                        </div>
+                        
+                        <div class="item_subtitle">
+                            Software Developer
+                            <div class="datetime">jan. 2017 - aug. 2019</div>
+                        </div>
+                    </div>
+
+                    <ul>
+                        <li>Programmed several escaperooms. (IJmuiden, Zandvoort, Veenendaal)</li>
+                        <li>Worked with a lot of Raspberry Pi, Arduino and different sensors.</li>
+                    </ul>
+                </div>
+
+                <div class="item">
+                    <div class="item_header">
+                        <div class="item_title">
+                            Landwinkel De Eekhoeve and Theehuis bij Kees & Sien
+                            <div class="location">Veenendaal, The Netherlands</div>
+                        </div>
+                        
+                        <div class="item_subtitle">
+                            Employee country shop and teahouse
+                            <div class="datetime">feb. 2013 - dec. 2016</div>
+                        </div>
+                    </div>
+
+                    <ul>
+                        <li>Responsible for the production of Christmas packaging for companies and individuals.</li>
+                        <li>Allround employee in the country shop.</li>
+                        <li>Teahouse employee with various functions such as: Host, Bartender and Service.</li>
+                    </ul>
+                </div>
+            </div>
+
+            <div class="section">
+                <div class="section_title">
+                    <span>Ext</span>racurricular<div class="section_title--bar"></div>
+                </div>
+
+                <div class="item">
+                    <div class="item_header">
+                        <div class="item_title">
+                            Time registration system
+                            <div class="location">Veenendaal, The Netherlands</div>
+                        </div>
+                        
+                        <div class="item_subtitle">
+                            Inventor & Programmer
+                            <div class="datetime">nov. 2015 - dec. 2016</div>
+                        </div>
+                    </div>
+
+                    <ul>
+                        <li>Check in/out system made for staff. A web interface on which staff can keep track of the hours they worked. Employer can see the availability of employees and schedule them based on that. An overview of each employee per month is also available.</li>
+                    </ul>
+                </div>
+            </div>
+
         </div>
     </body>
 </html>
